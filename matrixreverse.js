67b2635f267fd21cb5de7207a13be0af6ec52c5e
Dave Spina <dspina79@gmail.com>
2022-01-24 21:05:31 -0500

    Added matrix mirror

--- matrixreverse.js
+++ matrixreverse.js
@@ -7,5 +7,18 @@ function mirrorVector(v) {
     return newV;
 }
 
+function mirrorMatrix(m) {
+    let newM = [];
+    for(var i = 0; i < m.length; i++) {
+        newM.push(mirrorVector(m[i]));
+    }
+    return newM;
+}
+
+// [1,2,3], [4,5,6], [7,8,9]
+
 const v = [1,2,3,4,5];
 console.log(mirrorVector(v));
+
+const m = [[1,2,3], [4,5,6], [7,8,9]];
+console.log(mirrorMatrix(m));
